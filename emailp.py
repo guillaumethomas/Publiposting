8ecb313e20f3bf5d9145e881ef15d2b6202314bd
guillaumethomas <guillaumethomas@outlook.com>
2017-05-15 21:26:24 -0700

    error management & loop

--- emailp.py
+++ emailp.py
@@ -11,33 +11,31 @@ def main():
     gmail_user = 'guillaume.ronan.thomas@gmail.com'
     gmail_password = input('Password ?\n')
 
-    to = ["guillaumethomas@outlook.com","sterennr@hotmail.com"]
+    to = ["guillaumethomas@outlook.com", "guillaumethomas@att.net"]
     subject = "Test Python"
     content="Coucou Mon Panda\nJe fais un test en python\n \nBisous\nTon Loup"
 
-    server_t = connection_server(gmail_user,gmail_password)
-    if server_t[0] == True:
-        server = server_t[1]
+    server = connection_server(gmail_user,gmail_password)
 
-    msg = mail_o(content,subject,gmail_user,to)
+    for address in to:
+        msg = mail_o(content,subject,gmail_user,address)
+        # server.sendmail(sent_from, address, email_text)
+        server.send_message(msg)
 
-    #server.sendmail(sent_from, to, email_text)
-    server.send_message(msg)
     server.close()
+    exit("Email sent")
 
 
 def connection_server(username, password):
 
-    bool=True
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(username,password)
     except:
-        print('Something went wrong...')
-        bool=False
+        exit("Bad Connection or Bad Password => email not sent")
 
-    return bool, server
+    return server
 
 
 def mail_o(content, subject, sender, recipient):
